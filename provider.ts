caa378eb973c9a44a28a2ab1e13707984fafcd8f
devop <ffuentes@andain.cl>
2020-09-29 13:08:49 -0300

    test 1

---
+++ provider.ts
@@ -0,0 +1,17 @@
+// import { Injectable } from '@angular/core';
+// import { cordova, IonicNativePlugin } from '@ionic-native/core';
+
+// @Injectable()
+// export class Avram extends IonicNativePlugin {
+
+// 	static pluginName = 'cordova-plugin-avram';
+// 	static plugin = 'cordova-plugin-avram';
+// 	static pluginRef = 'Avram';
+// 	static repo = 'https://git.andain.cl/ffuentes/cordova-plugin-avram-andain.git';
+// 	static platforms = ['Android', 'iOS']
+
+
+// 	getAvailableRAM(): Promise<any> {
+// 		return cordova(this, 'getAvailableRAM', {}, []);
+// 	}
+// }
